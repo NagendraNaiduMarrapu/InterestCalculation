f0d154db68b2ee3ef3ac8b5029aacdfe7509dcc0
NagendraNaiduMarrapu <87158953+NagendraNaiduMarrapu@users.noreply.github.com>
2023-07-12 17:17:44 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,14 +15,13 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	private ArrayList<Transaction> lines;
-	private ArrayList<Transaction> accountTrans;
-	List<Account> list;
-	private double interest;
+	private static ArrayList<Transaction> lines;
+	private static ArrayList<Transaction> accountTrans;
+	static List<Account> list;
 
-	public void getAllTheAccounts() {
+	public static void getAllTheAccounts() {
 		try {
-			File file = new File("Account.txt");
+			File file = new File("D:\\account aggregator workspace\\MySpace\\InterestCalculator\\Account.txt");
 			Reader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String line;
@@ -43,9 +42,9 @@ public class Main {
 		}
 	}
 
-	public ArrayList<Transaction> readTransactionLines() {
+	public static ArrayList<Transaction> readTransactionLines() {
 		try {
-			File file = new File("C:\\Users\\vamsi pilla\\eclipse-workspace\\myprojects\\InterestCalculator\\Transaction.txt");
+			File file = new File("D:\\account aggregator workspace\\MySpace\\InterestCalculator\\Transaction.txt");
 			Reader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String line;
@@ -63,99 +62,161 @@ public class Main {
 			}
 
 			bufferedReader.close();
-			
+
 		} catch (IOException ex) {
 			System.out.println(ex);
 		}
 		return lines;
 	}
 
-	public ArrayList<Transaction> getAccounttransactions(Account a, ArrayList<Transaction> lines) {
-		accountTrans =new ArrayList<>();
-		for(int i=0;i<lines.size();i++) {
-			if(a.getAccNo() == lines.get(i).getAccNo()) {
+	public static ArrayList<Transaction> getAccounttransactions(Account a, ArrayList<Transaction> lines) {
+		accountTrans = new ArrayList<>();
+		for (int i = 0; i < lines.size(); i++) {
+			if (a.getAccNo() == lines.get(i).getAccNo()) {
 				accountTrans.add(lines.get(i));
 			}
 		}
 		return accountTrans;
-		
-			
-	
+
 	}
-	
-	
-	public double getBalanceAsOnDate(Account a,LocalDate givendate,ArrayList<Transaction> newt) {
-		double amt = a.getBalance() ;
+
+	public static double getBalanceAsOnDate(Account a, LocalDate givendate, ArrayList<Transaction> newt) {
+		double amt = a.getBalance();
 		ArrayList<Transaction> al = getAccounttransactions(a, newt);
-		Collections.sort(al,new Comparator<Transaction>() {
-			public int compare(Transaction t1,Transaction t2) {
+		Collections.sort(al, new Comparator<Transaction>() {
+			public int compare(Transaction t1, Transaction t2) {
 				return t2.getTdate().compareTo(t1.getTdate());
-				
+
 			}
 		});
-		
-		for(Transaction t : al) {
-		
-			System.out.println(t.toString());
-			if(t.getTdate().compareTo(givendate)>=0) {
-			if(t.getTtype().equals("W")) {
-				amt = amt+ t.getTamount();
-				
+
+		for (Transaction t : al) {
+
+			if (t.getTdate().compareTo(givendate) <= 0) {
+				if (t.getTtype().equals("W")) {
+					amt = amt + t.getTamount();
+
+				} else {
+					amt = amt - t.getTamount();
+				}
 			}
-			else {
-				amt = amt - t.getTamount();
-			}
-			}
-			
+
 		}
-		
 		return amt;
-		
+
 	}
-	
-	
-	public double CalInterest(Account a) {
-	 Period p=Period.between(LocalDate.now(),a.getOpenDate());
-	 int months=(int) p.toTotalMonths();
-	 if(a.accType == "sb" && a.getOpenDate().getDayOfMonth()>=10 && LocalDate.now().getMonthValue()==a.getOpenDate().getMonthValue()){
-			return 0;		
-	}
-     
-     else if(a.accType == "sb" && months<6 && a.getOpenDate().getDayOfMonth()>=10) {
-    	 for(int i=0;i<months-1;i++) {
-    		 
-    	 }
-     }
-	}
-	
-	
-	public double getMinBalance(Account a,int month,int year) {
+
+	public static double getMinBalance(Account a, int month, int year) {
 		ArrayList<Transaction> at = new ArrayList<Transaction>();
 		at = getAccounttransactions(a, lines);
-		double min = getBalanceAsOnDate(a,LocalDate.of(year, month, 10),at) ;
-		
-		for(Transaction t1:at) {
-			
+		double min1 = getBalanceAsOnDate(a, LocalDate.of(year, month, 10), at);
+		for (Transaction t1 : at) {
+
 			int currentMonth = t1.getTdate().getMonthValue();
-			if(month == currentMonth && t1.getTdate().getYear() == year && t1.getTdate().getDayOfMonth()>=10) {
-				double min2 = getBalanceAsOnDate(a,t1.getTdate(),at);
-				min = Math.min(min,min2);
+			if (month == currentMonth && t1.getTdate().getYear() == year && t1.getTdate().getDayOfMonth() >= 10) {
+				double min2 = getBalanceAsOnDate(a,LocalDate.now(), at);
+				min1 = Math.min(min1, min2);
+
 			}
 		}
-		System.out.println(min);
-		return min;
+
+		return min1;
+	}
+
+	public static double CalInterest(Account a) {
+		Period p = Period.between(a.getOpenDate(), LocalDate.now());
+		int months = (int) p.toTotalMonths();
+		System.out.println(months);
+		int currentmonth = LocalDate.now().getMonthValue();
+		int Currentyear = LocalDate.now().getYear();
+		if (currentmonth == 1) {
+			currentmonth = 12;
+			Currentyear -= 1;
+		} else {
+			currentmonth -= 1;
+		}
+
+		double interest = 0;
+		if (a.accType.equals("cd")) {
+			return 0;
+		} else if (a.accType.equals("sb") && a.getOpenDate().getDayOfMonth() >= 10
+				&& LocalDate.now().getMonthValue() == a.getOpenDate().getMonthValue()) {
+
+			return 0;
+		}
+
+		else if (a.accType.equals("sb") && months < 6 && a.getOpenDate().getDayOfMonth() >= 10) {
+
+			for (int i = 0; i < months - 1; i++) {
+				if (currentmonth >= 1) {
+
+					double CurrentMonthMinBalance = getMinBalance(a, currentmonth, Currentyear);
+					System.out.println(CurrentMonthMinBalance);
+					currentmonth = currentmonth - 1;
+					double CurrentMonthlyInterest = ((CurrentMonthMinBalance) * (4.5 / 12)) / 1200;
+					interest += CurrentMonthlyInterest;
+
+				} else {
+					double CurrentMonthMinBalance = getMinBalance(a, currentmonth, Currentyear);
+					System.out.println(CurrentMonthMinBalance);
+					currentmonth = 12;
+					Currentyear -= 1;
+					double CurrentMonthlyInterest = ((CurrentMonthMinBalance) * (4.5 / 12)) / 1200;
+					interest += CurrentMonthlyInterest;
+
+				}
+
+			}
+		}
+
+		else if (a.accType.equals("sb") && months < 6 && a.getOpenDate().getDayOfMonth() < 10) {
+
+			for (int i = 0; i < months; i++) {
+
+				if (currentmonth >= 1) {
+
+					double CurrentMonthMinBalance = getMinBalance(a, currentmonth, Currentyear);
+					System.out.println(CurrentMonthMinBalance);
+					currentmonth = currentmonth - 1;
+					double CurrentMonthlyInterest = ((CurrentMonthMinBalance) * (4.5 / 12)) / 1200;
+					interest += CurrentMonthlyInterest;
+
+				}
+			}
+		}
+
+		else if (a.accType.equals("sb") && months >= 6) {
+			for (int i = 0; i < 6; i++) {
+				if (currentmonth >= 1) {
+
+					double CurrentMonthMinBalance = getMinBalance(a, currentmonth, Currentyear);
+					System.out.println(CurrentMonthMinBalance);
+					currentmonth = currentmonth - 1;
+					double CurrentMonthlyInterest = ((CurrentMonthMinBalance) * (4.5 / 12)) / 1200;
+					interest += CurrentMonthlyInterest;
+
+				} else {
+					double CurrentMonthMinBalance = getMinBalance(a, currentmonth, Currentyear);
+					System.out.println(CurrentMonthMinBalance);
+					currentmonth = 12;
+					Currentyear -= 1;
+					double CurrentMonthlyInterest = ((CurrentMonthMinBalance) * (4.5 / 12)) / 1200;
+					interest += CurrentMonthlyInterest;
+
+				}
+			}
+		}
+		System.out.println(interest);
+		return interest;
 	}
 
 	public static void main(String[] args) {
-		Main m = new Main();
-		ArrayList<Transaction> alt = new ArrayList<>();
-		Account a1 = new Account(101,"sb",LocalDate.of(2023,02,9),50000);
-		alt = m.readTransactionLines();
-		
-		System.out.println(m.getBalanceAsOnDate(a1,LocalDate.of(2023,03,05),alt));
-		m.getMinBalance(a1, 4 , 2023);
+		getAllTheAccounts();
+		readTransactionLines();
+
+		getAccounttransactions(list.get(0), readTransactionLines());
+
+		CalInterest(list.get(0));
+
 	}
-
 }
-
-
